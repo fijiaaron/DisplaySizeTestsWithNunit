81903e1602b5367e114b856d060f80b0e075054e
Aaron Evans <aarone@one-shore.com>
2020-07-30 15:32:03 -0600

    create project

---
+++ DisplaySizeChromeTest.cs
@@ -0,0 +1,110 @@
+using NUnit.Framework;
+
+namespace DisplaySizeTestsWithNunit
+{
+	using NUnit.Framework;
+	using OpenQA.Selenium;
+	using OpenQA.Selenium.Chrome;
+	using System;
+
+	public class DisplaySizeChromeTest
+	{
+		IWebDriver driver;
+
+		private void log(String message)
+		{
+			TestContext.Out.WriteLine(message);
+		}
+
+		[SetUp]
+		public void Setup()
+		{
+			log("starting driver");
+			driver = new ChromeDriver();
+			log(driver.GetType().Name);
+
+			log("opening url");
+			driver.Url = "https://whatismyviewport.com/";
+			log(driver.Url);
+		}
+
+
+		[Test]
+		public void CheckUnmodifiedDimensions()
+		{
+			(int viewportWidth, int viewportHeight) = getViewportDimensions(driver);
+			(int screenWidth, int screenHeight) = getScreenDimensions(driver);
+
+			Assert.That(viewportWidth, Is.GreaterThanOrEqualTo(1280));
+			Assert.That(screenWidth, Is.GreaterThanOrEqualTo(1280));
+			Assert.That(screenWidth, Is.GreaterThan(viewportWidth));
+		}
+
+		[Test]
+		public void CheckMaximizedDimensions()
+		{
+			driver.Manage().Window.Maximize();
+
+			(int viewportWidth, int viewportHeight) = getViewportDimensions(driver);
+			(int screenWidth, int screenHeight) = getScreenDimensions(driver);
+
+			Assert.That(viewportWidth, Is.GreaterThanOrEqualTo(1280));
+			Assert.That(screenWidth, Is.GreaterThanOrEqualTo(1280));
+			Assert.That(screenWidth, Is.EqualTo(viewportWidth));
+		}
+
+
+		public (int x, int y) getViewportDimensions()
+		{
+			var js = (IJavaScriptExecutor)driver;
+
+			var x = Convert.ToInt32(js.ExecuteScript("return window.innerWidth"));
+			var y = Convert.ToInt32(js.ExecuteScript("return window.innerHeight"));
+
+			log("viewportWidth: " + x);
+			log("viewportHeight: " + y);
+
+			return (x, y);
+		}
+
+		public (int w, int h) getScreenDimensions(IWebDriver driver)
+		{
+			var js = (IJavaScriptExecutor)driver;
+
+			var w = Convert.ToInt32(js.ExecuteScript("return window.screen.width"));
+			var h = Convert.ToInt32(js.ExecuteScript("return window.screen.height"));
+
+			log("screen width: " + w);
+			log("screen height: " + h);
+
+			return (w, h);
+		}
+
+		public (int w, int h) getViewportDimensions(IWebDriver driver)
+		{
+			var js = (IJavaScriptExecutor)driver;
+
+			var w = Convert.ToInt32(js.ExecuteScript("return window.innerWidth"));
+			var h = Convert.ToInt32(js.ExecuteScript("return window.innerHeight"));
+
+			log("viewport width: " + w);
+			log("viewport height: " + h);
+
+			return (w, h);
+		}
+
+
+		[TearDown]
+		public void TearDown()
+		{
+			Pause(2);
+			driver.Quit();
+		}
+
+		private void Pause(float seconds)
+		{
+			int milliseconds = Convert.ToInt32(seconds * 1000);
+			System.Threading.Thread.Sleep(milliseconds);
+		}
+	}
+}
